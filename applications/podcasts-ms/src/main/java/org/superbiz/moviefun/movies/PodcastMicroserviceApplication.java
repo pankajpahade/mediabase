d0cd983785a144debebeec04aa282584adb58c7b
Pankaj Pahade <pankaj.pahade@perficient.com>
2019-04-30 16:11:59 +0530

    Created Microservices

---
+++ applications/podcasts-ms/src/main/java/org/superbiz/moviefun/movies/PodcastMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package org.superbiz.moviefun.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastMicroserviceApplication {
+    public static void main(String... args) {
+            SpringApplication.run(PodcastMicroserviceApplication.class, args);
+        }
+}
