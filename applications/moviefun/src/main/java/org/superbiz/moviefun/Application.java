6a5097c42d6f3acab7cd3a37ed6021ddb51daa17
Pankaj Pahade <pankaj.pahade@perficient.com>
2019-05-02 11:43:56 +0530

    Commit Lab5 Changes

--- applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
@@ -11,14 +13,15 @@ import org.superbiz.moviefun.moviesui.ActionServlet;
 import org.superbiz.moviefun.moviesui.MovieClient;
 import org.superbiz.moviefun.podcastsui.PodcastClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
-    @Value("${movies.ms.url")
+   /* @Value("${movies.ms.url")
     private String moviesURL;
 
     @Value("${podcasts.ms.url")
-    private String podcastsURL;
+    private String podcastsURL;*/
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
@@ -29,6 +32,7 @@ public class Application {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 
+    @LoadBalanced
     @Bean
     public RestOperations restOperations() {
         return new RestTemplate();
@@ -36,11 +40,11 @@ public class Application {
 
     @Bean
     public MovieClient movieClient(RestOperations restOperations) {
-        return new MovieClient(moviesURL, restOperations);
+        return new MovieClient("//movies-ms/movies", restOperations);
     }
 
     @Bean
     public PodcastClient podcastsClient(RestOperations restOperations) {
-        return new PodcastClient(podcastsURL, restOperations);
+        return new PodcastClient("//podcasts-ms/podcasts", restOperations);
     }
 }
