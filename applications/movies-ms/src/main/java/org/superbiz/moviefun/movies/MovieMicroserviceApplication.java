4e143b7474cd3b64117787fd2a0a3ef30052e066
Pankaj Pahade <pankaj.pahade@perficient.com>
2019-05-02 08:47:29 +0530

    Updated Microservices Test

--- applications/movies-ms/src/main/java/org/superbiz/moviefun/movies/MovieMicroserviceApplication.java
+++ applications/movies-ms/src/main/java/org/superbiz/moviefun/movies/MovieMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package org.superbiz.moviefun.podcasts;
+package org.superbiz.moviefun.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
