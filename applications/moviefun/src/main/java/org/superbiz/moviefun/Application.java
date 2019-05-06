28b14f1683dfd1d6fca09c4cbbabc11021eee4eb
Pankaj Pahade <pankaj.pahade@perficient.com>
2019-05-06 12:32:40 +0530

    Lab7 Test

--- applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
@@ -1,6 +1,5 @@
 package org.superbiz.moviefun;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
--- applications/movies-ms/src/main/java/org/superbiz/moviefun/movies/DisableSecurityConfiguration.java
+++ applications/movies-ms/src/main/java/org/superbiz/moviefun/movies/DisableSecurityConfiguration.java
@@ -1,10 +1,16 @@
 package org.superbiz.moviefun.movies;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
+@ConditionalOnProperty(value="enable-oauth", havingValue = "false")
 @EnableWebSecurity
 @Configuration
 public class DisableSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -12,6 +18,12 @@ public class DisableSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestOperations restOperations() {
+        return new RestTemplate();
     }
 
 }
